0a6d03f138a666edb88275596d50d31d70a01faa
sowonwow2 <sowonwow2@gmail.com>
2022-08-17 15:42:34 +0900

    🌟Init frontend

--- frontend/src/redux/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import userSlice from '../slices/user';
-
-const rootReducer = combineReducers({
-  user: userSlice.reducer,
-});
-
-export default rootReducer;
