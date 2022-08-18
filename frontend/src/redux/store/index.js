6032b00b87ba13e32fdae9bf277ae4a87de033e4
sowonwow2 <sowonwow2@gmail.com>
2022-08-18 11:02:23 +0900

    🔨Update Redux-devtools Settings

--- frontend/src/redux/store/index.js
+++ frontend/src/redux/store/index.js
@@ -1,15 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducer';
+import logger from "redux-logger";
 
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => {
-    if (__DEV__) {
-      const createDebugger = require('redux-flipper').default;
-      return getDefaultMiddleware().concat(createDebugger());
-    }
-    return getDefaultMiddleware();
-  },
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== "production", //  개발자 도구 on/off
+    // preloadedState: {}, // 스토어의 초기값
+    // 개발자가 원하는 store enhancer를 미들웨어가 적용되는 순서보다 앞서서 추가 가능
+    // enhancers: [reduxBatch],
 });
+
 
 export default store;
